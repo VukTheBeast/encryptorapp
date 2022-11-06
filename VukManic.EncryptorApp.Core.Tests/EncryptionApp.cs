e4ed430dd800bef3e296fe8d26171c4de3d654ed
Vuk Manic <ext-manicv@b2b.regn.net>
2022-11-06 23:05:08 +0100

    Encripted class move to file

---
+++ VukManic.EncryptorApp.Core.Tests/EncryptionApp.cs
@@ -0,0 +1,9 @@
+namespace VukManic.EncryptorApp.Core.Tests;
+
+public class EncryptionApp
+{
+    public object Encrypt(string stringTest)
+    {
+        throw new NotImplementedException();
+    }
+}
